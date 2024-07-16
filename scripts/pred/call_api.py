db362402f2604b63474f2c33480361316c8eb918
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2024-07-17 00:24:30 +0200

    bugfix

--- scripts/pred/call_api.py
+++ scripts/pred/call_api.py
@@ -287,8 +287,6 @@ def main():
     if len(batch):
         batched_data.append(batch)
 
-    print(len(data), len(batched_data), args.batch_size)
-
     # setting buffering=1 to force to dump the output after every line, so that we can see intermediate generations
     with open(pred_file, 'at', encoding="utf-8", buffering=1) as fout:
         # the data is processed sequentially, so we can store the start and end of current processing window
